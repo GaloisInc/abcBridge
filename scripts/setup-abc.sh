ce485201fddd6824c055a59a795674afe029dcc7
Aaron Tomb <atomb@galois.com>
2017-07-11 09:03:25 -0700

    Avoid the `--strip-components` flag to `tar`
    This doesn't seem to be supported by all versions.

--- scripts/setup-abc.sh
+++ scripts/setup-abc.sh
@@ -38,7 +38,7 @@ else
 	  # Unpack into the abc-build subdirectory
 	  # Note: some games are played to strip off the top-level directory name that
 	  # is automatically assigned by BitBucket
-	  mkdir -p abc-build && (cd abc-build; cat "../$LOCAL_TARBALL" | tar -x -z --strip-components=1)
+	  mkdir -p abc-build && (cd abc-build; cat "../$LOCAL_TARBALL" | tar -x -z; mv rdockins-abc-*/* .)
       fi
 
       # Interrogate the expected version number of the ABC sources
